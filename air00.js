641f12e39fc21adaa1227cb7a1bf7da5cdffcb71
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2023-09-30 11:39:51 +0200

    split et join a proscrire ?

---
+++ air00.js
@@ -0,0 +1,11 @@
+if (process.argv.length < 3) {
+    console.error('No argument provided');
+    process.exit(1);
+}
+
+function ma_fonction(string_à_couper, string_séparateur) { 
+    tableau = string_à_couper.split(string_séparateur)
+	return (tableau)
+}
+
+console.log(ma_fonction(process.argv[2], " ").join("\n"))
